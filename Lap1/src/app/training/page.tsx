08e9115cb98b0367e1ecdab84ab1da678f8db2db
nguyentienlong1106 <tienlong1106@gmail.com>
2023-11-14 17:30:27 +0300

    lap1

---
+++ Lap1/src/app/training/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Training = () => {
+  return (
+    <main className="lg:mx-[80px] lg:mt-[24px] ">
+      <section className="grid grid-cols-2 mx-[-80px] bg-training h-[680px]">
+        {" "}
+        nois{" "}
+      </section>{" "}
+    </main>
+  );
+};
+
+export default Training;
